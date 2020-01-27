295ffb0b6f721ed2c5da170fcaeaa9f9180f9c0b
Tuomas Haapala <tuomas.haapala@anders.fi>
2020-01-28 00:05:42 +0200

    Add initial dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,14 @@ import { Admin } from 'react-admin';
 import fakeDataProvider from 'ra-data-fakerest';
 
 import i18nProvider from './i18n/i18nProvider';
+import Dashboard from './Dashboard/Dashboard';
 
 const App: React.FC = () => (
-  <Admin dataProvider={fakeDataProvider({})} i18nProvider={i18nProvider} />
+  <Admin
+    dataProvider={fakeDataProvider({})}
+    i18nProvider={i18nProvider}
+    dashboard={Dashboard}
+  />
 );
 
 export default App;
