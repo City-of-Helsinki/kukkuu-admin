330b9867c4bda4edcd9b005b7c51940142fd4eb8
Tuomas Haapala <tuomas.haapala@anders.fi>
2020-01-27 23:59:18 +0200

    Add i18n support
    Supported languges are Finnish (currently enabled), Swedish and
    English.

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { Admin } from 'react-admin';
 import fakeDataProvider from 'ra-data-fakerest';
 
-const App: React.FC = () => <Admin dataProvider={fakeDataProvider({})} />;
+import i18nProvider from './i18n/i18nProvider';
+
+const App: React.FC = () => (
+  <Admin dataProvider={fakeDataProvider({})} i18nProvider={i18nProvider} />
+);
 
 export default App;
