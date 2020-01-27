538c0c62458f3ef63e1566a4a563935ab199a47e
Tuomas Haapala <tuomas.haapala@anders.fi>
2020-01-28 00:20:28 +0200

    Add some Helsinki colors
    Added Helsinki "coat of arms" and "summer" as the primary and secondary
    colors just to temporarily get a hinch of Helsinki feel while we wait
    for a proper theme.

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,13 @@ import fakeDataProvider from 'ra-data-fakerest';
 
 import i18nProvider from './i18n/i18nProvider';
 import Dashboard from './Dashboard/Dashboard';
+import theme from './theme';
 
 const App: React.FC = () => (
   <Admin
     dataProvider={fakeDataProvider({})}
     i18nProvider={i18nProvider}
+    theme={theme}
     dashboard={Dashboard}
   />
 );
