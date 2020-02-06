677930af19f58c54ad5f64381c6de015861d2cc0
Tuomas Haapala <tuomas.haapala@anders.fi>
2020-02-06 14:45:27 +0200

    Add initial venue list
    We use mock data for now to get something visible until the actual API
    integration is implemented.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,31 @@
 import React from 'react';
-import { Admin } from 'react-admin';
-import fakeDataProvider from 'ra-data-fakerest';
+import { Admin, Resource, useTranslate } from 'react-admin';
+import fakeDataProvider from 'ra-data-fakerest'; // TODO replace with an actual API data provider
+import PlaceIcon from '@material-ui/icons/Place';
 
 import i18nProvider from './i18n/i18nProvider';
 import Dashboard from './Dashboard/Dashboard';
+import VenueList from './venues/VenueList';
 import theme from './theme';
+import mockData from './mock_data';
 
-const App: React.FC = () => (
-  <Admin
-    dataProvider={fakeDataProvider({})}
-    i18nProvider={i18nProvider}
-    theme={theme}
-    dashboard={Dashboard}
-  />
-);
+const App: React.FC = () => {
+  const translate = useTranslate();
+  return (
+    <Admin
+      dataProvider={fakeDataProvider(mockData)}
+      i18nProvider={i18nProvider}
+      theme={theme}
+      dashboard={Dashboard}
+    >
+      <Resource
+        name="venues"
+        options={{ label: translate('venues.list.title') }}
+        icon={PlaceIcon}
+        list={VenueList}
+      />
+    </Admin>
+  );
+};
 
 export default App;
