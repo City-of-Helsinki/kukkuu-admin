f6dd3175bb59097a6ab7b030cd17f85f7dda6d22
Chi <chi.nguyenhuungoc@gmail.com>
2020-02-21 13:04:43 +0200

    remove wip test

--- src/api/__tests__/dataProvider.tests.tsx
+++
@@ -1,9 +0,0 @@
-import { TestContext } from 'react-admin';
-import { mount } from 'enzyme';
-
-
-describe('dataProvider', () => {
-    test('return defined result when handler exist for certain Resource', () => {
-        
-    })
-})
