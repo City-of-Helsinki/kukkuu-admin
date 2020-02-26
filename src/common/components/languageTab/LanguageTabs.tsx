c0d5f64854bb3422a20799accf82cc6c2d95a8d2
Chi Nguyen <chi.nguyen@digia.com>
2020-02-26 15:47:47 +0200

    remove unused styles, add doc

--- src/common/components/languageTab/LanguageTabs.tsx
+++ src/common/components/languageTab/LanguageTabs.tsx
@@ -1,44 +1,41 @@
 import * as React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import { useTranslate } from 'react-admin';
 
 import { CONTENT_LANGUAGES } from '../../constants';
-const useStyles = makeStyles({
-  tabsWrapper: {
-    fontWeight: 'bold',
-  },
-  tabs: {},
-  tabContent: {},
-});
 
+/**
+ * Use as a wrapper for translatable content view
+ * as render props, (please forgive me, HOC is worse).
+ *
+ * Example:
+ * <LanguageTabs>
+ *    {(selectedLanguage: string) =>
+ *      <FooBar selectedLanguage={selectedLanguage} />}
+ * </LanguageTabs>
+ *
+ */
 const LanguageTabs: React.FunctionComponent<{
   children: (selectedLocale: string) => React.ReactNode;
 }> = ({ children }) => {
   const [selectedLocale, selectLocale] = React.useState(CONTENT_LANGUAGES[0]);
-  const styles = useStyles();
   const translate = useTranslate();
 
   return (
-    <div className={styles.tabsWrapper}>
-      <div className={styles.tabs}>
-        <Tabs
-          value={selectedLocale}
-          onChange={(e, value) => selectLocale(value)}
-        >
-          {CONTENT_LANGUAGES.map((language, index) => (
-            <Tab
-              key={index}
-              value={language}
-              label={translate(`languageTabs.tab.${language}.label`)}
-            />
-          ))}
-        </Tabs>
+    <>
+      <Tabs value={selectedLocale} onChange={(e, value) => selectLocale(value)}>
+        {CONTENT_LANGUAGES.map((language, index) => (
+          <Tab
+            key={index}
+            value={language}
+            label={translate(`languageTabs.tab.${language}.label`)}
+          />
+        ))}
+      </Tabs>
 
-        <div className={styles.tabContent}>{children(selectedLocale)}</div>
-      </div>
-    </div>
+      <>{children(selectedLocale)}</>
+    </>
   );
 };
 
