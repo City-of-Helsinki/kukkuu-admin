d3baae38d6cc8d1b5ffc6d767bc943fc09504d3a
Chi Nguyen <chi.nguyen@digia.com>
2020-02-27 15:57:31 +0200

    make tabs same level with layout children

--- src/common/components/languageTab/LanguageTabs.tsx
+++ src/common/components/languageTab/LanguageTabs.tsx
@@ -3,38 +3,41 @@ import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import { useTranslate } from 'react-admin';
 
-import { CONTENT_LANGUAGES } from '../../constants';
-
+import { EventTranslationLanguageCode as LanguageEnum } from '../../../api/generatedTypes/globalTypes';
 /**
  * Use as a wrapper for translatable content view
  * as render props, (please forgive me, HOC is worse).
  *
- * Example:
- * <LanguageTabs>
- *    {(selectedLanguage: string) =>
- *      <FooBar selectedLanguage={selectedLanguage} />}
- * </LanguageTabs>
+ * Example usage:
+ * 1. Create local state using useState hook or component state
+ * 2. import <LanguageTabs>, pass the required props, remember to use language enum.
+ * 3. Basically render language tab and trigger onChange props that passed in
+ * Gonna save a bit of boilterplate code.
+ * Unfortunately HOC can not be done cause React-admin only pass props down to 1 level deep of children.
  *
  */
 const LanguageTabs: React.FunctionComponent<{
-  children: (selectedLocale: string) => React.ReactNode;
-}> = ({ children }) => {
-  const [selectedLocale, selectLocale] = React.useState(CONTENT_LANGUAGES[0]);
+  selectedLanguage: LanguageEnum;
+  onSelect: (value: LanguageEnum) => void;
+}> = ({ selectedLanguage, onSelect }) => {
   const translate = useTranslate();
 
   return (
     <>
-      <Tabs value={selectedLocale} onChange={(e, value) => selectLocale(value)}>
-        {CONTENT_LANGUAGES.map((language, index) => (
-          <Tab
-            key={index}
-            value={language}
-            label={translate(`languageTabs.tab.${language}.label`)}
-          />
-        ))}
+      <Tabs value={selectedLanguage} onChange={(e, value) => onSelect(value)}>
+        <Tab
+          value={LanguageEnum.EN}
+          label={translate(`languageTabs.tab.EN.label`)}
+        />
+        <Tab
+          value={LanguageEnum.FI}
+          label={translate(`languageTabs.tab.FI.label`)}
+        />
+        <Tab
+          value={LanguageEnum.SV}
+          label={translate(`languageTabs.tab.SV.label`)}
+        />
       </Tabs>
-
-      <>{children(selectedLocale)}</>
     </>
   );
 };
--- src/domain/venues/VenueShow.tsx
+++ src/domain/venues/VenueShow.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
-import { Show, SimpleShowLayout, TextField, useTranslate } from 'react-admin';
+import React, { useState } from 'react';
+import { Show, TextField, useTranslate, SimpleShowLayout } from 'react-admin';
 
 import LanguageTabs from '../../common/components/languageTab/LanguageTabs';
+import { EventTranslationLanguageCode as LanguageEnum } from '../../api/generatedTypes/globalTypes';
 
 const VenueTitle = ({ record }: { record?: any }) => {
   return <span>{record ? `${record.translations.FI.name}` : ''}</span>;
@@ -9,43 +10,42 @@ const VenueTitle = ({ record }: { record?: any }) => {
 
 const VenueShow = (props: any) => {
   const translate = useTranslate();
+  const [selectedLanguage, selectLanguage] = useState(LanguageEnum.FI);
+  const translation = `translations.${selectedLanguage}`;
 
   return (
-    <LanguageTabs>
-      {(selectedLocale: string) => {
-        const translation = `translations.${selectedLocale}`;
-        return (
-          <Show title={<VenueTitle />} {...props}>
-            <SimpleShowLayout>
-              <TextField
-                source={`${translation}.name`}
-                label={translate('venues.fields.name.label')}
-              />
-              <TextField
-                source={`${translation}.address`}
-                label={translate('venues.fields.address.label')}
-              />
-              <TextField
-                source={`${translation}.description`}
-                label={translate('venues.fields.description.label')}
-              />
-              <TextField
-                source={`${translation}.accessibilityInfo`}
-                label={translate('venues.fields.accessibilityInfo.label')}
-              />
-              <TextField
-                source={`${translation}.arrivalInstructions`}
-                label={translate('venues.fields.arrivalInstructions.label')}
-              />
-              <TextField
-                source={`${translation}.additionalInformation`}
-                label={translate('venues.fields.additionalInformation.label')}
-              />
-            </SimpleShowLayout>
-          </Show>
-        );
-      }}
-    </LanguageTabs>
+    <Show title={<VenueTitle />} {...props}>
+      <SimpleShowLayout>
+        <LanguageTabs
+          selectedLanguage={selectedLanguage}
+          onSelect={selectLanguage}
+        />
+        <TextField
+          source={`${translation}.name`}
+          label={translate('venues.fields.name.label')}
+        />
+        <TextField
+          source={`${translation}.address`}
+          label={translate('venues.fields.address.label')}
+        />
+        <TextField
+          source={`${translation}.description`}
+          label={translate('venues.fields.description.label')}
+        />
+        <TextField
+          source={`${translation}.accessibilityInfo`}
+          label={translate('venues.fields.accessibilityInfo.label')}
+        />
+        <TextField
+          source={`${translation}.arrivalInstructions`}
+          label={translate('venues.fields.arrivalInstructions.label')}
+        />
+        <TextField
+          source={`${translation}.additionalInformation`}
+          label={translate('venues.fields.additionalInformation.label')}
+        />
+      </SimpleShowLayout>
+    </Show>
   );
 };
 
