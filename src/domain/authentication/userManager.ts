542814faef91c40e7ad423c3c8d2c4b6f48527eb
Bernt Andreas Drange <bernt.andreas.drange@digia.com>
2020-06-15 15:10:38 +0300

    Log user out if token expires

--- src/domain/authentication/userManager.ts
+++ src/domain/authentication/userManager.ts
@@ -1,15 +1,19 @@
 import { createUserManager } from 'redux-oidc';
 import { UserManagerSettings, Log, WebStorageStateStore } from 'oidc-client';
+import * as Sentry from '@sentry/browser';
 
 import { fetchApiToken } from './api';
+import authProvider from './authProvider';
 
 const location = `${window.location.protocol}//${window.location.hostname}${
   window.location.port ? `:${window.location.port}` : ''
 }`;
 
-// Show oidc debugging info in the console - should only be active on development
-Log.logger = console;
-Log.level = Log.INFO;
+if (process.env.NODE_ENV === 'development') {
+  // Show oidc debugging info in the console - should only be active on development
+  Log.logger = console;
+  Log.level = Log.INFO;
+}
 
 /* eslint-disable @typescript-eslint/camelcase */
 const settings: UserManagerSettings = {
@@ -19,7 +23,7 @@ const settings: UserManagerSettings = {
   client_id: process.env.REACT_APP_OIDC_CLIENT_ID,
   redirect_uri: `${location}/callback`,
   // For debugging, set it to 1 minute by removing comment:
-  accessTokenExpiringNotificationTime: 59.65 * 60,
+  // accessTokenExpiringNotificationTime: 59.65 * 60,
   automaticSilentRenew: false,
   silent_redirect_uri: `${location}/silent_renew.html`,
   response_type: 'id_token token',
@@ -31,22 +35,23 @@ const settings: UserManagerSettings = {
 const userManager = createUserManager(settings);
 
 userManager.events.addAccessTokenExpiring(async () => {
-  console.count('addAccessTokenExpiring');
   try {
-    console.count('addAccessTokenExpiring - signinSilent');
+    console.count('userManager addAccessTokenExpiring fetchApiToken');
     const newUser = await userManager.signinSilent();
     await userManager.storeUser(newUser);
     const apiToken = await fetchApiToken(newUser.access_token);
-    console.count('userManager addAccessTokenExpired fetchApiToken');
     localStorage.setItem('apiToken', apiToken);
-    console.count('setting fetchingApiToken in localstorage to 0');
   } catch (error) {
-    console.error('addAccessTokenExpired in expiring caught error');
+    // This happens if you're offline for example, seems responsible to log out.
+    authProvider.logout({});
+    Sentry.captureException(error);
+    // eslint-disable-next-line no-console
     console.error(error);
   }
 });
 
 userManager.events.addSilentRenewError((error) => {
+  // eslint-disable-next-line no-console
   console.error('userManager addSilentRenewError', error);
 });
 
@@ -56,14 +61,15 @@ userManager.events.addUserSignedOut(async () => {
   try {
     await userManager.signoutRedirect();
   } catch (error) {
+    // eslint-disable-next-line no-console
     console.error(error);
+    Sentry.captureException(error);
   }
 });
 
 userManager.events.addAccessTokenExpired(() => {
-  // TODO: Decide whether this is a good idea.
   console.count('addAccessTokenExpired');
-  // authProvider.logout({});
+  authProvider.logout({});
 });
 
 export default userManager;
