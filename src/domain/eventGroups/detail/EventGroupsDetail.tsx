7dfb82839c555917991fb13094e2317af131e4a7
Jaakko Jokinen <jaakko.jokinen@teamit.fi>
2020-11-12 07:19:11 +0200

    Simplify app imports

---
+++ src/domain/eventGroups/detail/EventGroupsDetail.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const EventGroupsDetail = () => {
+  return <>EventGroupsDetail</>;
+};
+
+export default EventGroupsDetail;
---
+++ src/domain/venues/VenuesResource.ts
@@ -0,0 +1,11 @@
+import VenueList from './VenueList';
+import VenueShow from './VenueShow';
+import VenueEdit from './VenueEdit';
+import VenueCreate from './VenueCreate';
+
+export default {
+  List: VenueList,
+  Detail: VenueShow,
+  Edit: VenueEdit,
+  Create: VenueCreate,
+};
