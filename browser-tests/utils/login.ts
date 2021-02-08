659ef0375e9fb7dedbed4d1023d29c2dbe96557a
Jaakko Jokinen <jaakko.jokinen@teamit.fi>
2021-02-08 14:57:55 +0200

    Run browser tests against PR envs

--- browser-tests/utils/login.ts
+++ browser-tests/utils/login.ts
@@ -2,19 +2,30 @@ import { ssoLogin } from '../pages/ssoLogin';
 import { login as loginPage } from '../pages/login';
 import { testUsername, testUserPassword } from './settings';
 
+const givePermission = async (t: TestController) => {
+  // If the user is show a permission request page
+  if (await ssoLogin.permissionPage.exists) {
+    // Give permission
+    await t.click(ssoLogin.givePermissionButton);
+  }
+};
+
 type Options = {
   username?: string;
   password?: string;
-}
+};
 
 export const login = async (t: TestController, options: Options = {}) => {
-  const { username = testUsername(), password = testUserPassword()} = options;
+  const { username = testUsername(), password = testUserPassword() } = options;
   await t
     .click(loginPage.loginButton)
     .click(ssoLogin.loginLink)
     .typeText(ssoLogin.username, username)
     .typeText(ssoLogin.password, password)
-    .click(ssoLogin.loginButton)
-    // Wait for authorization to finish
-    .wait(3000); // 3s
+    .click(ssoLogin.loginButton);
+
+  await givePermission(t);
+
+  // Wait for authorization to finish
+  await t.wait(3000); // 3s
 };
