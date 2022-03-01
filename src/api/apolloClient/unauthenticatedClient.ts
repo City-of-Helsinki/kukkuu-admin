6a67653be8336449735078e2dcd5576f068e2c60
Jaakko Jokinen <jaakko.jokinen@teamit.fi>
2022-03-01 16:52:56 +0200

    Add missing http link

--- src/api/apolloClient/unauthenticatedClient.ts
+++ src/api/apolloClient/unauthenticatedClient.ts
@@ -1,4 +1,4 @@
-import { ApolloClient, ApolloLink } from '@apollo/client';
+import { ApolloClient, ApolloLink, HttpLink } from '@apollo/client';
 import { InMemoryCache } from '@apollo/client/cache';
 import { onError } from '@apollo/client/link/error';
 
@@ -6,8 +6,12 @@ import handleApolloError from './handleApolloError';
 
 const errorLink = onError(handleApolloError);
 
+const httpLink = new HttpLink({
+  uri: process.env.REACT_APP_API_URI,
+});
+
 const unauthenticatedClient = new ApolloClient({
-  link: ApolloLink.from([errorLink]),
+  link: ApolloLink.from([errorLink, httpLink]),
   defaultOptions: {
     watchQuery: {
       fetchPolicy: 'no-cache',
