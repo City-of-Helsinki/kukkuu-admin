5106ca5add0233b14c2a118c6f038cea3d823940
Tero Virtanen <tero.virtanen@mavericks.fi>
2023-08-01 12:48:29 +0300

    chore: browser test login language to English

--- browser-tests/utils/login.ts
+++ browser-tests/utils/login.ts
@@ -2,6 +2,12 @@ import { ssoLogin } from '../pages/ssoLogin';
 import { login as loginPage } from '../pages/login';
 import { testUsername, testUserPassword } from './settings';
 
+const selectLoginLanguage = async (t: TestController, language: string) => {
+  await t.click(ssoLogin.localeDropdown);
+  // select correct language from dropdown list
+  await t.click(ssoLogin.localeDropdown.find('li').withText(language));
+};
+
 const givePermission = async (t: TestController) => {
   // If the user is show a permission request page
   if (await ssoLogin.permissionPage.exists) {
@@ -19,7 +25,12 @@ export const login = async (t: TestController, options: Options = {}) => {
   const { username = testUsername(), password = testUserPassword() } = options;
   await t
     .click(loginPage.loginButton)
-    .click(ssoLogin.loginLink)
+    .click(ssoLogin.loginLink);
+
+  // select locale English
+  await selectLoginLanguage(t, ssoLogin.localeLanguage);
+
+  await t
     .typeText(ssoLogin.username, username)
     .typeText(ssoLogin.password, password)
     .click(ssoLogin.loginButton);
