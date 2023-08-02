1c20a3c633c9c458f748fe4489f693d1a9a3ae75
Tero Virtanen <105207219+terovirtanen@users.noreply.github.com>
2023-08-02 10:07:19 +0300

    chore: browser test login language to English (#237)
    * chore: browser test login language to English
    * chore: permission page texts in Finnish

--- browser-tests/pages/ssoLogin.ts
+++ browser-tests/pages/ssoLogin.ts
@@ -1,10 +1,13 @@
 import { screen } from '@testing-library/testcafe';
+import { Selector } from 'testcafe';
 
 export const ssoLogin = {
   loginLink: screen.getByText('Helsinki-tunnus'),
   username: screen.getByLabelText('Email'),
   password: screen.getByLabelText('Password'),
   loginButton: screen.getByDisplayValue('Log In'),
-  permissionPage: screen.queryByText('Permission request'),
-  givePermissionButton: screen.getByDisplayValue('Allow'),
+  permissionPage: screen.queryByText('Älä anna lupaa'),
+  givePermissionButton: screen.getByDisplayValue('Anna lupa'),
+  localeDropdown: Selector('#kc-locale-dropdown'),
+  localeLanguage: 'English',
 };
