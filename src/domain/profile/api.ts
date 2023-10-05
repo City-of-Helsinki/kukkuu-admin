1700a7913226ba28cf26f9e0475d21569b459613
Niko Lindroos <niko.j.makela@gmail.com>
2023-10-05 14:46:29 +0300

    fix: toolbar projects dropdown
    KK-1017

--- src/domain/profile/api.ts
+++ src/domain/profile/api.ts
@@ -2,14 +2,19 @@ import { ApolloQueryResult } from '@apollo/client';
 
 import { queryHandler, handleApiNode } from '../../api/utils/apiUtils';
 import { myAdminProfileQuery } from './queries';
-import { MyAdminProfile as MyAdminProfileQueryResult } from '../../api/generatedTypes/MyAdminProfile';
+import {
+  MyAdminProfile as MyAdminProfileQueryResult,
+  MyAdminProfile_myAdminProfile,
+} from '../../api/generatedTypes/MyAdminProfile';
 
 const getMyAdminProfile = async () => {
   const response: ApolloQueryResult<MyAdminProfileQueryResult> =
     await queryHandler({
       query: myAdminProfileQuery,
     });
-  return handleApiNode(response.data.myAdminProfile);
+  return handleApiNode<MyAdminProfile_myAdminProfile>(
+    response.data.myAdminProfile
+  );
 };
 
 export { getMyAdminProfile };
