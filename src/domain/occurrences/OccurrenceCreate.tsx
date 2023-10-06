29861e3cc969e7b3e3cd0c25e015c703fa8fe8b8
Niko Lindroos <niko.j.makela@gmail.com>
2023-10-06 11:34:26 +0300

    fix!: the record context is available only inside the ra-view
    KK-1017

--- src/domain/occurrences/OccurrenceCreate.tsx
+++ src/domain/occurrences/OccurrenceCreate.tsx
@@ -12,32 +12,39 @@ import { useParams } from 'react-router-dom';
 import DateTimeTextInput from '../../common/components/dateTimeTextField/DateTimeTextField';
 import { OccurrenceCapacityOverrideInput } from './inputs';
 
+const OccurrenceCreateForm = () => {
+  const { event_id: eventId } = useParams();
+  return (
+    <SimpleForm defaultValues={{ eventId }}>
+      <DateTimeTextInput variant="outlined" required />
+      <ReferenceInput
+        label="occurrences.fields.venue.label"
+        source="venueId"
+        reference="venues"
+        validate={[required()]}
+        fullWidth
+      >
+        <SelectInput
+          variant="outlined"
+          optionText="translations.FI.name"
+          helperText="occurrences.fields.venue.helperText"
+        />
+      </ReferenceInput>
+      <OccurrenceCapacityOverrideInput eventId={eventId} />
+    </SimpleForm>
+  );
+};
+
 const OccurrenceCreate = () => {
   const { event_id: eventId } = useParams();
-
   return (
     <Grid container direction="column" xs={6} item>
       <Create
         title="occurrences.create.title"
+        // FIXME: What is the show/1- URL? Questioned in KK-1017.
         redirect={eventId ? `/events/${eventId}/show/1` : 'show'}
       >
-        <SimpleForm defaultValues={{ eventId }}>
-          <DateTimeTextInput variant="outlined" required />
-          <ReferenceInput
-            label="occurrences.fields.venue.label"
-            source="venueId"
-            reference="venues"
-            validate={[required()]}
-            fullWidth
-          >
-            <SelectInput
-              variant="outlined"
-              optionText="translations.FI.name"
-              helperText="occurrences.fields.venue.helperText"
-            />
-          </ReferenceInput>
-          <OccurrenceCapacityOverrideInput eventId={eventId} />
-        </SimpleForm>
+        <OccurrenceCreateForm />
       </Create>
     </Grid>
   );
