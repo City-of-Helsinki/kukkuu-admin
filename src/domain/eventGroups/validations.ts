da1c804350dad245b83992ccbf9dba95a5a73245
Niko Lindroos <niko.j.makela@gmail.com>
2023-10-09 12:38:32 +0300

    refactor: event groups form level validation with YUP
    KK-1017.
    
    "Validation: Form Level & Input Level Are Mutually Exclusive
    
    With react-hook-form, you can’t have both form level validation and input level validation. This is because form level validation is meant to be used for schema based validation.
    
    If you used form level validation to run complex checks for multiple input values combinations, you can use a schema library such as yup."
    
    https://marmelab.com/react-admin/Upgrade.html#validation-form-level--input-level-are-mutually-exclusive

--- src/domain/eventGroups/validations.ts
+++ src/domain/eventGroups/validations.ts
@@ -1,6 +1,8 @@
 import { required, maxLength } from 'react-admin';
+import { object, string } from 'yup';
 
 import { requireFinnishFields } from '../../common/utils';
+import { Language } from '../../api/generatedTypes/globalTypes';
 
 export const validateName = required();
 
@@ -12,3 +14,21 @@ export const validateEventGroupForm = (values: any) => {
     'eventGroups.translations.FI.name.required',
   ]);
 };
+
+export const getEventGroupsTranslatedFieldsSchema = (lang: Language) =>
+  object({
+    name:
+      lang === Language.FI
+        ? string().required('eventGroups.translations.FI.name.required')
+        : string(),
+    description: string(),
+    shortDescription: string(),
+  });
+
+export const eventGroupsSchema = object({
+  translations: object({
+    [Language.FI]: getEventGroupsTranslatedFieldsSchema(Language.FI),
+    [Language.SV]: getEventGroupsTranslatedFieldsSchema(Language.SV),
+    [Language.EN]: getEventGroupsTranslatedFieldsSchema(Language.EN),
+  }),
+});
