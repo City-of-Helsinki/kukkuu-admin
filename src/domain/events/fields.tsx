dca9f450b438064ebebcd6562e811d82c1c04b93
Niko Lindroos <niko.j.makela@gmail.com>
2023-10-10 16:10:16 +0300

    fix: published field -component
    KK-1017

--- src/domain/events/fields.tsx
+++ src/domain/events/fields.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import Typography from '@mui/material/Typography';
-import { useTranslate } from 'react-admin';
+import { useRecordContext, useTranslate } from 'react-admin';
 
 import { Events_events_edges_node as Event } from '../../api/generatedTypes/Events';
+import { Language } from '../../api/generatedTypes/globalTypes';
 
 export const PublishedField = ({
-  locale,
-  record,
+  locale = Language.FI,
 }: {
-  locale: any;
-  record?: Event;
+  locale?: string;
 }) => {
+  const record = useRecordContext<Event>();
   const translate = useTranslate();
   if (!record) return null;
   const date = new Date(record.publishedAt);
