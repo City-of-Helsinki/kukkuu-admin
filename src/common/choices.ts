92e8f26323c2c9e1b84f9d534d7d8d02b1eca3cd
Niko Lindroos <niko.j.makela@gmail.com>
2024-01-23 13:02:39 +0200

    fix: child language choices field value rendering
    KK-1076

--- src/common/choices.ts
+++ src/common/choices.ts
@@ -1,6 +1,6 @@
 import { Language } from '../domain/api/generatedTypes/graphql';
 
 export const languageChoices = Object.keys(Language).map((language) => ({
-  id: language,
-  name: `languages.${language}`,
+  id: language.toUpperCase(),
+  name: `languages.${language.toUpperCase()}`,
 }));
