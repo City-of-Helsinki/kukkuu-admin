1ab643900b5e27dd1516badd2e8f4c3d402ae5ab
Niko Lindroos <niko.j.makela@gmail.com>
2024-10-10 12:46:47 +0300

    feat: arrival status and mark as attended on ticket verification page
    KK-1224

--- src/domain/ticketValidation/useVerifyTicketQuery.ts
+++ src/domain/ticketValidation/useVerifyTicketQuery.ts
@@ -1,22 +1,14 @@
-import { gql, useQuery } from '@apollo/client';
+import { useQuery } from '@apollo/client';
 
-const verifyTicketQuery = gql`
-  query VerifyTicket($referenceId: String!) {
-    verifyTicket(referenceId: $referenceId) {
-      occurrenceTime
-      eventName
-      venueName
-      validity
-    }
-  }
-`;
+import type { VerifyTicketQuery } from '../api/generatedTypes/graphql';
+import { VerifyTicketDocument } from '../api/generatedTypes/graphql';
 
 type Config = {
   referenceId: string;
 };
 
 export default function useVerifyTicketQuery({ referenceId }: Config) {
-  return useQuery(verifyTicketQuery, {
+  return useQuery<VerifyTicketQuery>(VerifyTicketDocument, {
     skip: !referenceId,
     variables: { referenceId },
   });
