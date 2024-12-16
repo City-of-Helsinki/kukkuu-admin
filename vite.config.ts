ee62efb0711dee7a84ab164415d3e4d806281b22
Niko Lindroos <niko.j.makela@gmail.com>
2024-12-16 15:15:13 +0200

    fix: build and deps issues
    KK-1017

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
-import eslint from 'vite-plugin-eslint';
 
 export default ({ mode }: any) => {
   // Load app-level env vars to node-level env vars.
@@ -12,7 +11,7 @@ export default ({ mode }: any) => {
     define: {
       'process.env': process.env,
     },
-    plugins: [react(), eslint(), viteTsconfigPaths()],
+    plugins: [react(), viteTsconfigPaths()],
     server: {
       open: true, // automatically open the app in the browser
       port: parseInt(process.env.PORT ?? '3001'),
