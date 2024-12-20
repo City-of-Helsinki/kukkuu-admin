d2bad8390025058a909a214e00a895114e2d8d9e
Niko Lindroos <niko.j.makela@gmail.com>
2024-12-20 11:58:41 +0200

    fix: eslint installation
    KK-1371

---
+++ .eslintrc.cjs
@@ -0,0 +1,65 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    'vitest-globals/env': true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'plugin:@vitest/legacy-recommended',
+    'plugin:vitest-globals/recommended',
+    'plugin:@typescript-eslint/stylistic',
+  ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  ignorePatterns: ['**/generatedTypes/*', '**/generated/*'],
+  plugins: ['import', 'react', 'react-hooks', '@vitest', 'prettier'],
+  rules: {
+    '@typescript-eslint/brace-style': [
+      'error',
+      '1tbs',
+      { allowSingleLine: true },
+    ],
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/func-call-spacing': ['error'],
+    '@typescript-eslint/member-ordering': 'warn',
+    '@typescript-eslint/no-require-imports': 'error',
+    '@typescript-eslint/strict-boolean-expressions': 'off',
+    '@typescript-eslint/restrict-template-expressions': 'off',
+    '@typescript-eslint/no-unsafe-argument': 'off',
+    '@typescript-eslint/consistent-type-imports': [
+      'error',
+      {
+        disallowTypeAnnotations: false,
+        fixStyle: 'separate-type-imports',
+        prefer: 'type-imports',
+      },
+    ],
+    '@typescript-eslint/no-floating-promises': 'off',
+    '@typescript-eslint/consistent-type-definitions': 'off',
+    'react/no-unused-prop-types': ['warn', { skipShapeProps: true }],
+    'array-bracket-spacing': ['warn', 'never'],
+    'no-prototype-builtins': 'off',
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin',
+          'external',
+          ['internal', 'parent', 'sibling', 'index'],
+        ],
+        'newlines-between': 'always',
+      },
+    ],
+    'max-len': ['warn', { code: 120 }],
+    'no-console': 'warn',
+    'no-plusplus': 'error',
+    'no-undef': 'warn',
+    'object-curly-spacing': ['warn', 'always'],
+  },
+};
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
+import eslint from 'vite-plugin-eslint';
 
 export default ({ mode }: any) => {
   // Load app-level env vars to node-level env vars.
@@ -11,7 +12,7 @@ export default ({ mode }: any) => {
     define: {
       'process.env': process.env,
     },
-    plugins: [react(), viteTsconfigPaths()],
+    plugins: [react(), eslint(), viteTsconfigPaths()],
     server: {
       open: true, // automatically open the app in the browser
       port: parseInt(process.env.PORT ?? '3001'),
