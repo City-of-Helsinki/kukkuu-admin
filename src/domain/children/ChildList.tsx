b8d9347b397d8f145ed978993d383801766054fe
Niko Lindroos <niko.j.makela@gmail.com>
2025-01-28 11:35:42 +0200

    refactor: remove needless canViewFamilies check from Child views
    KK-1403.
    
    The canViewFamilies permissions checked on React-admin root level and
    the whole child resource is unloaded when the permission is not given.
    For that reason, it does not make sense to recheck the permission in the
    child views.

--- src/domain/children/ChildList.tsx
+++ src/domain/children/ChildList.tsx
@@ -7,62 +7,49 @@ import {
   FunctionField,
   useTranslate,
   Pagination,
-  usePermissions,
 } from 'react-admin';
 import { CardHeader } from '@mui/material';
 
 import { languageChoices } from '../../common/choices';
 import KukkuuList from '../application/layout/kukkuuListPage/KukkuuList';
 import type { ChildNode } from '../api/generatedTypes/graphql';
-import type { Permissions } from '../authentication/authProvider';
-import projectService from '../projects/projectService';
 
 const ChildList = () => {
-  const { permissions } = usePermissions<Permissions>();
   const translate = useTranslate();
-  const projectId = projectService.projectId ?? undefined;
-  const canViewFamilies = Boolean(
-    permissions?.canViewFamiliesWithinProject?.(projectId)
-  );
 
   return (
     <>
       <CardHeader title={translate('children.list.title')} />
-      {canViewFamilies ? (
-        <KukkuuList
-          exporter={false}
-          pagination={<Pagination rowsPerPageOptions={[25, 100]} />}
-          perPage={25}
-        >
-          <Datagrid rowClick="show" bulkActionButtons={false}>
-            <FunctionField
-              label="children.fields.name.label"
-              render={(record?: Partial<ChildNode>) =>
-                record?.name?.trim() ?? ''
-              }
-            />
-            <TextField
-              source="birthyear"
-              label="children.fields.birthyear.label"
-            />
-            <TextField
-              source="postalCode"
-              label="children.fields.guardians.fields.postalCode.label"
-            />
-            <EmailField
-              source="guardians.edges.0.node.email"
-              label="children.fields.guardians.label"
-            />
-            <SelectField
-              source="guardians.edges.0.node.language"
-              label="events.fields.language.label"
-              choices={languageChoices}
-            />
-          </Datagrid>
-        </KukkuuList>
-      ) : (
-        <div>{translate('children.noPermission')}</div>
-      )}
+
+      <KukkuuList
+        exporter={false}
+        pagination={<Pagination rowsPerPageOptions={[25, 100]} />}
+        perPage={25}
+      >
+        <Datagrid rowClick="show" bulkActionButtons={false}>
+          <FunctionField
+            label="children.fields.name.label"
+            render={(record?: Partial<ChildNode>) => record?.name?.trim() ?? ''}
+          />
+          <TextField
+            source="birthyear"
+            label="children.fields.birthyear.label"
+          />
+          <TextField
+            source="postalCode"
+            label="children.fields.guardians.fields.postalCode.label"
+          />
+          <EmailField
+            source="guardians.edges.0.node.email"
+            label="children.fields.guardians.label"
+          />
+          <SelectField
+            source="guardians.edges.0.node.language"
+            label="events.fields.language.label"
+            choices={languageChoices}
+          />
+        </Datagrid>
+      </KukkuuList>
     </>
   );
 };
--- src/domain/children/ChildShow.tsx
+++ src/domain/children/ChildShow.tsx
@@ -13,8 +13,6 @@ import {
   SimpleShowLayout,
   TextField,
   useLocaleState,
-  usePermissions,
-  useRecordContext,
   useTranslate,
 } from 'react-admin';
 import { CardHeader } from '@mui/material';
@@ -23,19 +21,10 @@ import { languageChoices } from '../../common/choices';
 import OccurrenceTimeRangeField from '../occurrences/fields/OccurrenceTimeRangeField';
 import KukkuuShow from '../application/layout/kukkuuDetailPage/KukkuuShow';
 import type { ChildNode } from '../api/generatedTypes/graphql';
-import type { Permissions } from '../authentication/authProvider';
-import projectService from '../projects/projectService';
 
 const ChildShow = () => {
-  const { permissions } = usePermissions<Permissions>();
-  const record = useRecordContext<ChildNode>();
   const translate = useTranslate();
   const [locale] = useLocaleState();
-  const projectId =
-    record?.project?.id ?? projectService.projectId ?? undefined;
-  const canViewFamilies = Boolean(
-    permissions?.canViewFamiliesWithinProject?.(projectId)
-  );
   const onClickGuardian = (record: ChildNode) =>
     record &&
     `${record.guardians.edges[0]?.node?.firstName} ${record.guardians.edges[0]?.node?.lastName}`.trim();
@@ -52,74 +41,68 @@ const ChildShow = () => {
   return (
     <>
       <CardHeader title={translate('children.show.title')} />
-      {canViewFamilies ? (
-        <KukkuuShow title="children.show.title">
-          <SimpleShowLayout>
-            <FunctionField
-              label="children.fields.name.label"
-              render={(record?: Partial<ChildNode>) =>
-                record?.name?.trim() ?? ''
-              }
-            />
-            <TextField
-              source="birthyear"
-              label="children.fields.birthyear.label"
-            />
-            <SelectField
-              source="guardians.edges.0.node.language"
-              label="events.fields.language.label"
-              choices={languageChoices}
-            />
-            <TextField
-              source="postalCode"
-              label="children.fields.guardians.fields.postalCode.label"
-            />
-            <FunctionField
-              label="children.fields.guardians.label"
-              render={onClickGuardian}
-            />
-            <EmailField
-              source="guardians.edges.0.node.email"
-              label="children.fields.guardians.fields.email.label"
-            />
-            <TextField
-              source="guardians.edges.0.node.phoneNumber"
-              label="children.fields.guardians.fields.phoneNumber.label"
-            />
-            <ArrayField
-              source="occurrences.edges"
-              label="children.fields.occurrences.label"
-            >
-              <Datagrid bulkActionButtons={false} rowClick={onClickOccurrence}>
-                <DateField
-                  label="occurrences.fields.time.fields.date.label"
-                  source="node.time"
-                  locales={locale}
-                />
-                <OccurrenceTimeRangeField occurrenceSource="node" />
-                <ReferenceField
-                  label="occurrences.fields.event.label"
-                  source="node.event.id"
-                  reference="events"
-                  link="show"
-                >
-                  <TextField source="translations.FI.name" />
-                </ReferenceField>
-                <ReferenceField
-                  label="occurrences.fields.venue.label"
-                  source="node.venue.id"
-                  reference="venues"
-                  link="show"
-                >
-                  <TextField source="translations.FI.name" />
-                </ReferenceField>
-              </Datagrid>
-            </ArrayField>
-          </SimpleShowLayout>
-        </KukkuuShow>
-      ) : (
-        <div>{translate('children.noPermission')}</div>
-      )}
+      <KukkuuShow title="children.show.title">
+        <SimpleShowLayout>
+          <FunctionField
+            label="children.fields.name.label"
+            render={(record?: Partial<ChildNode>) => record?.name?.trim() ?? ''}
+          />
+          <TextField
+            source="birthyear"
+            label="children.fields.birthyear.label"
+          />
+          <SelectField
+            source="guardians.edges.0.node.language"
+            label="events.fields.language.label"
+            choices={languageChoices}
+          />
+          <TextField
+            source="postalCode"
+            label="children.fields.guardians.fields.postalCode.label"
+          />
+          <FunctionField
+            label="children.fields.guardians.label"
+            render={onClickGuardian}
+          />
+          <EmailField
+            source="guardians.edges.0.node.email"
+            label="children.fields.guardians.fields.email.label"
+          />
+          <TextField
+            source="guardians.edges.0.node.phoneNumber"
+            label="children.fields.guardians.fields.phoneNumber.label"
+          />
+          <ArrayField
+            source="occurrences.edges"
+            label="children.fields.occurrences.label"
+          >
+            <Datagrid bulkActionButtons={false} rowClick={onClickOccurrence}>
+              <DateField
+                label="occurrences.fields.time.fields.date.label"
+                source="node.time"
+                locales={locale}
+              />
+              <OccurrenceTimeRangeField occurrenceSource="node" />
+              <ReferenceField
+                label="occurrences.fields.event.label"
+                source="node.event.id"
+                reference="events"
+                link="show"
+              >
+                <TextField source="translations.FI.name" />
+              </ReferenceField>
+              <ReferenceField
+                label="occurrences.fields.venue.label"
+                source="node.venue.id"
+                reference="venues"
+                link="show"
+              >
+                <TextField source="translations.FI.name" />
+              </ReferenceField>
+            </Datagrid>
+          </ArrayField>
+        </SimpleShowLayout>
+      </KukkuuShow>
     </>
   );
 };
