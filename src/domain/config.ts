da7540db83b20bf2d83098c7b45c4b9f473bdcf7
Kari Salminen <kari.salminen@anders.com>
2025-01-31 14:24:47 +0200

    chore: enable external ticket systems, remove related feature flag
    remove `VITE_FEATURE_FLAG_EXTERNAL_TICKET_SYSTEM_SUPPORT` feature flag
    as unnecessary, it's been enabled in testing/staging/production for
    quite a while already
    
    refs KK-1412 (noticed this unnecessary feature flag while testing)

--- src/domain/config.ts
+++ src/domain/config.ts
@@ -14,13 +14,6 @@ class Config {
   static get IS_TEST_ENVIRONMENT() {
     return Boolean(import.meta.env.VITE_IS_TEST_ENVIRONMENT);
   }
-
-  static get featureFlagExternalTicketSystemSupport() {
-    return (
-      import.meta.env.VITE_FEATURE_FLAG_EXTERNAL_TICKET_SYSTEM_SUPPORT ===
-      'true'
-    );
-  }
 }
 
 export default Config;
