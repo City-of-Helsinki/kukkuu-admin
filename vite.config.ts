7fbd16f108ed3794f6229dbd42647f686171890f
timwessman <tim.wessman@gofore.com>
2026-09-10 10:46:39 +0300

    feat: generate third-party license notices at build time
    Refs: KK-1495

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,14 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import eslint from '@nabla/vite-plugin-eslint';
 
+import { thirdPartyLicenses } from './scripts/thirdPartyLicenses';
+
 export default ({ mode }: any) => {
   // Load app-level env vars to node-level env vars.
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
   return defineConfig({
     envPrefix: 'VITE_',
-    plugins: [react(), eslint()],
+    plugins: [react(), eslint(), thirdPartyLicenses()],
     server: {
       open: true, // automatically open the app in the browser
       port: parseInt(process.env.PORT ?? '3001'),
